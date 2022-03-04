4e3a99147eb80411c1f816e237baeb1b719bfa33
SpiderPigzz <Ricky.winarko@gmail.com>
2022-03-04 23:39:27 +0800

    Edited Main Menu and Filter Menu

--- Eyedylic Prototype/Assets/Scripts/PauseMenu.cs
+++ Eyedylic Prototype/Assets/Scripts/PauseMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -11,10 +12,22 @@ public class PauseMenu : MonoBehaviour
     public Button DRButt, MacularButt, CataractButt, GlaucomaButt;
     public Color selectColor;
     public GameObject FilterMenuObject;
+    
 
     public void OpenFilterMenu(){
         FilterMenuObject.SetActive(true);
         gameObject.SetActive(false);
+    }
+
+    public void ReturnButton()
+    {
+        ControlManager.ShowMenu = false;
+        gameObject.SetActive(false);
+    }
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("Main menu");
     }
 
 
